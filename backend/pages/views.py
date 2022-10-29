4ef7917302c26278e56678020df0f9f9851132df
Sudhan Regmi <xenonrazels@gmail.com>
2022-10-29 14:13:07 +0545

    Completing pages & static files for django app

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -9,3 +9,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name='pages/about.html'
+
+class ContactPageView(TemplateView):
+    template_name='pages/contact.html'
