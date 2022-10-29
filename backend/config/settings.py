de3502650b1a875e49e4abb8c5d8ae63903e4675
Sudhan Regmi <xenonrazels@gmail.com>
2022-10-29 13:53:48 +0545

    setting pages app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,25 +11,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jbijpi8l#ki5=+doc0z%zobdps$&vzd5dh01d+jdpfvvhh22*z'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -117,7 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT=[str(BASE_DIR.joinpath('static'))]
+MEDIA_URL='media/'
+MEDIA_ROOT=[str(BASE_DIR.joinpath('media'))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name='home.html'
+    template_name='pages/home.html'
 
 
 class AboutPageView(TemplateView):
-    template_name='about.html'
+    template_name='pages/about.html'
