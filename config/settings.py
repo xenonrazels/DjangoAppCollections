4c1a94deb16fe6022ee34a931e1d9d513e61d6de
Sudhan Regmi <xenonrazels@gmail.com>
2022-10-30 10:07:45 +0545

    Ready deploy

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,5 @@ MEDIA_ROOT=str(BASE_DIR.joinpath('media'))
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_on_heroku
+django_on_heroku=django_on_heroku.settings(locals())
